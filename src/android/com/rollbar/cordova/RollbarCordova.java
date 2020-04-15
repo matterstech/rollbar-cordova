d41f2158210bb0cf09dc334ad0c4e7b3c0c93077
Julien Desbois <julien.desbois@matters.tech>
2020-04-15 16:55:38 +0200

    refs #120820 use rollbar-java into rollbar-cordova (instead of rollbar-android that is deprecated)

--- src/android/com/rollbar/cordova/RollbarCordova.java
+++ src/android/com/rollbar/cordova/RollbarCordova.java
@@ -11,10 +11,43 @@ import org.json.JSONObject;
 import org.json.JSONException;
 
 import android.content.Context;
-import com.rollbar.android.Rollbar;
+import android.content.pm.PackageInfo;
+
+import com.rollbar.api.payload.data.Client;
+import com.rollbar.api.payload.data.Level;
+import com.rollbar.api.payload.data.Notifier;
+import com.rollbar.api.payload.data.Person;
+import com.rollbar.api.payload.data.Request;
+import com.rollbar.api.payload.data.Server;
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.notifier.config.ConfigBuilder;
+import com.rollbar.notifier.filter.Filter;
+import com.rollbar.notifier.fingerprint.FingerprintGenerator;
+import com.rollbar.notifier.provider.Provider;
+import com.rollbar.notifier.sender.Sender;
+import com.rollbar.notifier.transformer.Transformer;
+import com.rollbar.notifier.uuid.UuidGenerator;
+
+import java.net.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 public class RollbarCordova extends CordovaPlugin {
     private Rollbar rollbar;
+
+    public static final String TAG = "Rollbar";
+
+    private  String accessToken;
+    private  String environment;
+    private int versionCode;
+    private String versionName;
+    private String packageName;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -22,12 +55,39 @@ public class RollbarCordova extends CordovaPlugin {
 
         Context context = this.cordova.getActivity().getApplicationContext();
 
-        rollbar = Rollbar.init(context);
+        try {
+            this.packageName = context.getPackageName();
+            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, 0);
+
+            this.versionCode = info.versionCode;
+            this.versionName = info.versionName;
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Error getting package info.");
+        }
+
+        List<String> appPackages = Arrays.asList(packageName);
+        this.accessToken = webView.getPreferences().getString("rollbarclienttoken", "");
+        Config config = ConfigBuilder.withAccessToken(this.accessToken).codeVersion(this.versionName).appPackages(appPackages).build();
+
+        rollbar = Rollbar.init(config);
+
+
+
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("sendJsonPayload".equals(action)) {
+            Config config = rollbar.config();
+
+            JSONObject j = new JSONObject(args.getString(0));
+
+            this.environment = j.getJSONObject("data").getString("environment");
+            Config runtimeConfig = ConfigBuilder.withConfig(config).environment(this.environment).build();
+
+            rollbar.configure(runtimeConfig);
             rollbar.sendJsonPayload(args.getString(0));
 
             return true;
